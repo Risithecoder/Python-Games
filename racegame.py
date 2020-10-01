9780cf8d38075509f0ae6fe7fcad49ffb0f37fc8
RishiDharshan <rishidharshan@gmail.com>
2020-10-01 13:33:24 +0530

    Done bro

--- racegame.py
+++ racegame.py
@@ -44,34 +44,38 @@ class CircuitGenomeConfig(object):
                 ConfigParameter('compatibility_weight_coefficient', float),
                 ConfigParameter('conn_add_prob', float),
                 ConfigParameter('conn_delete_prob', float),
-                ConfigParameter('node_add_prob', float),
-                ConfigParameter('node_delete_prob', float)]
 
-    def __init__(self, params):
-        # Create full set of available activation functions.
-        self.activation_defs = ActivationFunctionSet()
-        self.activation_options = params.get('activation_options', 'sigmoid').strip().split()
-        self.aggregation_options = params.get('aggregation_options', 'sum').strip().split()
+    @classmethod#include<bits/stdc++.h>
+using namespace std;
 
-        # Gather configuration data from the gene classes.
-        self.__params += CircuitNodeGene.get_config_params()
-        self.__params += CircuitConnectionGene.get_config_params()
+#define long long ll
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-        # Use the configuration data to interpret the supplied parameters.
-        for p in self.__params:
-            setattr(self, p.name, p.interpret(params))
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
 
-        # By convention, input pins have negative keys, and the output
-        # pins have keys 0,1,...
-        self.input_keys = [-i - 1 for i in range(self.num_inputs)]
-        self.output_keys = [i for i in range(self.num_outputs)]
+	int test = 1;
 
-    def save(self, f):
-        write_pretty_params(f, self, self.__params)
+	//cin >> test;
+
+	while (test--) {
 
 
-class CircuitGenome(object):
-    @classmethod
+		//Here you go
+
+
+	}
+
+
+
+
+	return 0;
+}
+
     def parse_config(cls, param_dict):
         return CircuitGenomeConfig(param_dict)
 
